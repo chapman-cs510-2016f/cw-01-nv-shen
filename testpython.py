80b4383f989bb49eb91555e7cc7b9899a04710bf
helen77 <zny_ok1989@hotmail.com>
2016-09-02 07:13:53 +0000

    Add testpython.py

--- testpython.py
+++ testpython.py
@@ -1,5 +1,16 @@
-def main():
-	print('Hello Wold, Change me.')
+import random
 
+
+def main():
+	scores = random.randint(0,100)
+	
+	if 100 >= scores >= 90:
+		print('A')
+	elif 90 > scores >= 80:
+		print('B')
+	elif 80 > scores >=60:
+		print('C')
+	else:
+		print('D')
 
 main()
