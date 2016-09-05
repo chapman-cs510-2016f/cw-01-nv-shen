1834fb091ce7ddeda2772a8de07962dcbb099435
Alexis Ford <ford128@mail.chapman.edu>
2016-09-05 23:02:34 +0000

    Changed testpython.py

--- testpython.py
+++ testpython.py
@@ -1,16 +1,2 @@
-import random
-
-
-def main():
-	scores = random.randint(0,100)
-	
-	if 100 >= scores >= 90:
-		print('A')
-	elif 90 > scores >= 80:
-		print('B')
-	elif 80 > scores >=60:
-		print('C')
-	else:
-		print('D')
-
-main()
+def test_trivial():
+	assert True
